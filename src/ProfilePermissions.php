ff17f8494955ff87c4db8c3570c57b1ec6f791d4
Deji Akala <dejiakala@gmail.com>
2015-10-08 11:31:20 +0100

    Rename  to  for consistency

--- src/ProfilePermissions.php
+++ src/ProfilePermissions.php
@@ -42,32 +42,32 @@ class ProfilePermissions {
    *   An array of permission names and descriptions.
    */
   protected function buildPermissions(ProfileType $profile_type) {
-    $type_id = $profile_type->id();
+    $id = $profile_type->id();
     $type_params = ['%type' => $profile_type->label()];
 
     return [
-      "add own $type_id profile" => [
+      "add own $id profile" => [
         'title' => $this->t('%type: Add own profile', $type_params),
       ],
-      "add any $type_id profile" => [
+      "add any $id profile" => [
         'title' => $this->t('%type: Add any profile', $type_params),
       ],
-      "view own $type_id profile" => [
+      "view own $id profile" => [
         'title' => $this->t('%type: View own profile', $type_params),
       ],
-      "view any $type_id profile" => [
+      "view any $id profile" => [
         'title' => $this->t('%type: View any profile', $type_params),
       ],
-      "edit own $type_id profile" => [
+      "edit own $id profile" => [
         'title' => $this->t('%type: Edit own profile', $type_params),
       ],
-      "edit any $type_id profile" => [
+      "edit any $id profile" => [
         'title' => $this->t('%type: Edit any profile', $type_params),
       ],
-      "delete own $type_id profile" => [
+      "delete own $id profile" => [
         'title' => $this->t('%type: Delete own profile', $type_params),
       ],
-      "delete any $type_id profile" => [
+      "delete any $id profile" => [
         'title' => $this->t('%type: Delete any profile', $type_params),
       ],
     ];
